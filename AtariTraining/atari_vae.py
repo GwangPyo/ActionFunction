4714b38ecb52c75b4c297e4e4a7a1bdf6de7a89a
GwangPyo <31953954+GwangPyo@users.noreply.github.com>
2019-12-25 17:24:41 +0900

    modified to adjust API

--- AtariTraining/atari_vae.py
+++ AtariTraining/atari_vae.py
@@ -4,7 +4,7 @@ from stable_baselines.common.policies import CnnPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv
 
 if __name__ == "__main__":
-    env = SubprocVecEnv([lambda: gym.make("Breakout-v0") for _ in range(24)])
+    env = SubprocVecEnv([lambda: gym.make("Breakout-v0") for _ in range(16)])
     model = PPO2(CnnPolicy, env, verbose=1, n_steps=256)
     model.learn(10000000)
     model.save("PPO_breakout.pkl")
